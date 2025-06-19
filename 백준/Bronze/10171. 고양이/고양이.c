1fe3664758c642c69b968add43ff69fff46e9cda
abcu7832 <abcu7832@gmail.com>
2025-06-19 20:16:50 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,8 @@
+int main() {
+	printf("%c    %c%c\n", 92, 47, 92);
+    printf(" %c  %c %c%c\n", 41, 40, 39, 41);
+    printf("%c  %c  %c \n", 40, 47, 41);
+    printf(" %c%c%c%c%c%c \n", 92, 40, 95, 95, 41, 124);
+	return 0;
+}
+
