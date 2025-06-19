b82360f8bd6a0f313604c1e4fbb22e872e96d213
abcu7832 <abcu7832@gmail.com>
2025-06-19 19:54:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,11 @@
+int main(){
+    int i, j, N;
+    scanf("%d", &N);
+    for(i=0;i<N;i++){
+        for(j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
